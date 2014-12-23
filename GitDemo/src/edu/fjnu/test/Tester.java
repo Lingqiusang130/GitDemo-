f7ca4b40a695a89a03b753a9b0afcc3f42f014ac
Lingqiusang130 <137590332@qq.com>
2014-12-23 21:10:07 +0800

    测试版本初始化

---
+++ GitDemo/src/edu/fjnu/test/Tester.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.fjnu.test;
+
+/**
+ * @author lingqiusang
+ *
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("我是小红");
+	}
+
+}
